f555e9c8280a2831b3c539020841614a8068b694
Andres Colonia <andreskal@gmail.com>
2017-03-29 16:55:43 -0300

    rails cookbook

---
+++ attributes/rails.rb
@@ -0,0 +1,3 @@
+default["rails"]['ruby']["rbenv"] = "https://github.com/rbenv/rbenv.git"
+default["rails"]['ruby']["ruby_build"] = "https://github.com/rbenv/ruby-build.git"
+
---
+++ recipes/rails.rb
@@ -0,0 +1,59 @@
+#
+# Cookbook:: rails
+# Recipe:: rbenv
+#
+# rbenv
+git "#{node.normal["home"]}/.rbenv" do
+  repository node["rails"]["ruby"]["rbenv"]
+  user node.normal["user"]
+  group group node.normal["user"]
+end
+
+execute "rbenv" do
+  cwd node.normal["home"]
+  command <<-EOH
+  echo 'export PATH="#{node.normal["home"]}/.rbenv/bin:$PATH"' >> .bashrc
+  echo 'eval "$(rbenv init -)"' >> .bashrc
+  exec $SHELL
+  EOH
+end
+
+# ruby-build
+directory "#{node.normal["home"]}/.rbenv/plugins" do
+  owner node.normal["user"]
+  group node.normal["user"]
+  mode '0755'
+  action :create
+end
+
+git "#{node.normal["home"]}/.rbenv/plugins/ruby-build" do
+  repository node["rails"]["ruby"]["ruby_build"]
+  user node.normal["user"]
+  group node.normal["user"]
+end
+
+execute "ruby_build" do
+  cwd node.normal["home"]
+  command <<-EOH
+  echo 'export PATH="#{node.normal["home"]}/.rbenv/plugins/ruby-build/bin:$PATH"' >> .bashrc
+  exec $SHELL
+  EOH
+end
+
+# install rails
+execute "ruby_with_rails" do
+  user node.normal["user"]
+  group node.normal["user"]
+  environment({
+    "PATH" => "#{node.normal["home"]}/.rbenv/bin:#{node.normal["home"]}/.rbenv/plugins/ruby-build/bin:#{node.normal["home"]}/.rbenv/shims:#{ENV['PATH']}",
+    "HOME" => node.normal["home"]
+  })
+
+  command <<-EOH
+  rbenv install #{node.normal["ruby_version"]} --skip-existing
+  rbenv global #{node.normal["ruby_version"]}
+  gem install bundler
+  gem install rails -v #{node.normal['rails_version']}
+  EOH
+end
+
