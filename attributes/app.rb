d4a9f12556abc32acdedcdad8ca4ff084247104b
Andres Colonia <andreskal@gmail.com>
2017-03-30 15:15:22 -0300

    app recipe with puma deploy

---
+++ attributes/app.rb
@@ -0,0 +1,7 @@
+default['app']['puma']['folders'] = %w{log sockets pids}
+default['app']['puma']['workers'] = 4
+default['app']['puma']['threads'] = 16, 32
+default['app']['puma']['bind'] = "puma.sock"
+default['app']['puma']['pid'] = "puma.pid"
+default['app']['puma']['state'] = "puma.state"
+default['app']['puma']['logs'] = { access: "puma.access.log", error: "puma.error.log" }
---
+++ recipes/app.rb
@@ -0,0 +1,67 @@
+# Cookbook:: project
+# Recipe:: app
+#
+# get project
+git "#{node.normal["path"]}" do
+  user node.normal["user"]
+  group node.normal["user"]
+  repository node["app_source"]["url"]
+end
+
+execute "change permission" do
+  user "root"
+  command "chown -R #{node.normal["user"]}:#{node.normal["user"]} #{node.normal["path"]}"
+  action :run
+end
+
+# bundle install
+execute "bundle install" do
+  cwd node.normal["path"]
+  environment({
+    "PATH" => "#{node.normal["home"]}/.rbenv/bin:#{node.normal["home"]}/.rbenv/plugins/ruby-build/bin:#{node.normal["home"]}/.rbenv/shims:#{ENV["PATH"]}",
+    "HOME" => node.normal["home"]
+  })
+end
+
+# puma config
+
+#folders
+node["app"]["puma"]["folders"].each do |dir|
+  directory "#{node.normal["path"]}/#{dir}" do
+    mode 0755
+    owner node.normal["user"]
+    group node.normal["user"]
+    action :create
+    recursive true
+  end
+end
+
+# puma config file
+template "#{node.normal["path"]}/config/puma.rb" do
+  source "puma/puma.rb.erb"
+  owner node.normal["user"]
+  group node.normal["user"]
+  mode 0775
+  variables(
+    path: node.normal["path"],
+    workers: node["app"]["puma"]["workers"],
+    threads: node["app"]["puma"]["threads"],
+    folders: node["app"]["puma"]["folders"],
+    bind: node["app"]["puma"]["bind"],
+    pid: node["app"]["puma"]["pid"],
+    state: node["app"]["puma"]["state"],
+    logs: node["app"]["puma"]["logs"]
+  )
+end
+
+# puma start
+execute "bundle exec puma" do
+  cwd node.normal["path"]
+  user node.normal["user"]
+  group node.normal["user"]
+  environment({
+    "PATH" => "#{node.normal["home"]}/.rbenv/bin:#{node.normal["home"]}/.rbenv/plugins/ruby-build/bin:#{node.normal["home"]}/.rbenv/shims:#{ENV["PATH"]}",
+    "HOME" => node.normal["home"]
+  })
+  command "bundle exec puma -C config/puma.rb"
+end
