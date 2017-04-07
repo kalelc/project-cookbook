575f38c34b91e97d486f3dbfa1c32895b22499ce
Andres Colonia <andreskal@gmail.com>
2017-04-07 12:40:02 -0300

    hotfix attributes

--- attributes/app.rb
+++ attributes/app.rb
@@ -1,5 +1,5 @@
 default['app']['puma']['file'] = "config/puma.rb"
-default['app']['puma']['folders'] = %w{log sockets pids}
+default['app']['puma']['folders'] = %w(log sockets pids)
 default['app']['puma']['workers'] = 4
 default['app']['puma']['threads'] = 16, 32
 default['app']['puma']['bind'] = "puma.sock"
